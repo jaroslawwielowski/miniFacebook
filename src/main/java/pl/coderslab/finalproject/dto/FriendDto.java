d7bd3b30c1b5089cb7dd485853100b8d893c5ffa
Jarosław Wielowski <jaroslawwielowski@gmail.com>
2019-01-24 23:22:27 +0100

    user entity with post and group

--- src/main/java/pl/coderslab/finalproject/dto/FriendDto.java
+++ src/main/java/pl/coderslab/finalproject/dto/FriendDto.java
@@ -1,77 +1,77 @@
-//package pl.coderslab.finalproject.dto;
-//
-//import pl.coderslab.finalproject.entity.FriendId;
-//
-//import javax.persistence.*;
-//import java.io.Serializable;
-//import java.util.Date;
-//import java.util.Objects;
-//
-//
-//public class Friend implements Serializable {
-//
-//
-//
-//    @EmbeddedId
-//    private FriendId id;
-//
-//
-//    private UserDto user1;
-//
-//
-//    private User user2;
-//
-//
-//    private Date createdOn = new Date();
-//
-//    private boolean accepted;
-//
-//    public Friend() {
-//    }
-//
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(user1, user2);
-//    }
-//
-//    public FriendId getId() {
-//        return id;
-//    }
-//
-//    public void setId(FriendId id) {
-//        this.id = id;
-//    }
-//
-//    public User getUser1() {
-//        return user1;
-//    }
-//
-//    public void setUser1(User user1) {
-//        this.user1 = user1;
-//    }
-//
-//    public User getUser2() {
-//        return user2;
-//    }
-//
-//    public void setUser2(User user2) {
-//        this.user2 = user2;
-//    }
-//
-//    public Date getCreatedOn() {
-//        return createdOn;
-//    }
-//
-//    public void setCreatedOn(Date createdOn) {
-//        this.createdOn = createdOn;
-//    }
-//
-//    public boolean isAccepted() {
-//        return accepted;
-//    }
-//
-//    public void setAccepted(boolean accepted) {
-//        this.accepted = accepted;
-//    }
-//}
+package pl.coderslab.finalproject.dto;
+
+import pl.coderslab.finalproject.entity.FriendId;
+
+import java.util.Date;
+
+
+public class FriendDto {
+
+
+
+    private FriendId id;
+
+
+    private UserDto user1;
+
+
+    private UserDto user2;
+
+
+    private Date createdOn = new Date();
+
+    private boolean accepted;
+
+    public FriendDto() {
+    }
+
+
+
+    public FriendId getId() {
+        return id;
+    }
+
+    public void setId(FriendId id) {
+        this.id = id;
+    }
+
+    public FriendDto(FriendId id, UserDto user1, UserDto user2, Date createdOn, boolean accepted) {
+        this.id = id;
+        this.user1 = user1;
+        this.user2 = user2;
+        this.createdOn = createdOn;
+        this.accepted = accepted;
+    }
+
+    public UserDto getUser1() {
+        return user1;
+    }
+
+    public void setUser1(UserDto user1) {
+        this.user1 = user1;
+    }
+
+    public UserDto getUser2() {
+        return user2;
+    }
+
+    public void setUser2(UserDto user2) {
+        this.user2 = user2;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+}
--- src/main/java/pl/coderslab/finalproject/dto/MessagesDto.java
+++ src/main/java/pl/coderslab/finalproject/dto/MessagesDto.java
@@ -1,70 +1,51 @@
-//package pl.coderslab.finalproject.dto;
-//
-//import org.hibernate.annotations.CreationTimestamp;
-//
-//import javax.persistence.*;
-//import javax.validation.constraints.Max;
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//
-//@Entity
-//@Table(name = "messages")
-//public class Messages {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Max(1600)
-//    @Column(columnDefinition = "TEXT")
-//    private String message;
-//
-//    @CreationTimestamp
-//    private LocalDateTime createDateTime;
-//
-//    @ManyToMany//(mappedBy = "usergroup")
-//    private List<User> userList;
-//
-//    public Messages() {
-//    }
-//
-//    public Messages(@Max(1600) String message, LocalDateTime createDateTime, List<User> userList) {
-//        this.message = message;
-//        this.createDateTime = createDateTime;
-//        this.userList = userList;
-//    }
-//
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
-//
-//    public LocalDateTime getCreateDateTime() {
-//        return createDateTime;
-//    }
-//
-//    public void setCreateDateTime(LocalDateTime createDateTime) {
-//        this.createDateTime = createDateTime;
-//    }
-//
-//    public List<User> getUserList() {
-//        return userList;
-//    }
-//
-//    public void setUserList(List<User> userList) {
-//        this.userList = userList;
-//    }
-//}
+package pl.coderslab.finalproject.dto;
+
+
+import java.time.LocalDateTime;
+
+
+public class MessagesDto {
+
+
+    private Long id;
+
+    private String message;
+
+    private LocalDateTime createDateTime;
+
+
+
+    public MessagesDto() {
+    }
+
+    public MessagesDto(String message, LocalDateTime createDateTime) {
+        this.message = message;
+        this.createDateTime = createDateTime;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreateDateTime() {
+        return createDateTime;
+    }
+
+    public void setCreateDateTime(LocalDateTime createDateTime) {
+        this.createDateTime = createDateTime;
+    }
+
+}
